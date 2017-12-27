0a817289a57bc806ee4ad9e913683e54d0b54c36
Piyush Raj <piyushraj.4680@gmail.com>
2017-12-27 20:11:54 +0000

    Beta Version of StarFOSS :+1:

---
+++ Releases/StarFOSS/v1/runSwitch.js
@@ -0,0 +1,8 @@
+function runSwitchjs() {
+  alert("After clicking on 'Ok', Starring will start, leave the tab open, we'll let you know after the starring process. Feel free to jump on another tabs :)");
+  chrome.tabs.executeScript({
+    file: 'star.user.js'
+  });
+}
+
+document.getElementById('clickme').addEventListener('click', runSwitchjs);
